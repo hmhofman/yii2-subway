7faec09b368a1a74165c0e0450e8b806e0ad5aaa
Herman M. Hofman <herman.hofman@gmail.com>
2021-10-26 20:22:46 +0200

    Initial Commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=subway',
+    'username' => 'root',
+    'password' => 'dbUser4new',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
